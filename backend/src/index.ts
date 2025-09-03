dd9ce4a743f70864b1649fe4d18d2ebc9997f802
DanyBarrueto <dfbarrueto@hotmail.com>
2025-09-02 20:21:07 -0500

    V0
    Creación carpetas backend y fronted junto con la instalación de node.js

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import * as http from 'http';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('¡Hola Mundo desde TypeScript y Node.js!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
